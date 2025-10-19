6a5ed19cb8d908f58da401e27d134417697534a0
Chuck Skoda <charles.skoda@gmail.com>
2025-10-19 15:07:23 -0700

    Add offline support.

--- src/main.js
+++ src/main.js
@@ -2,6 +2,21 @@ import { Store } from './stores/store.js'
 import { Render } from './render.js'
 import { Input } from './input.js'
 
+async function registerServiceWorker() {
+  const sw = navigator.serviceWorker
+  if (sw && sw.controller !== null) return
+  const reg = await sw.register('/serviceWorker.js')
+  reg.addEventListener('updatefound', () => {
+    const regSW = reg.installing
+    // Reload on first activating service worker so all files cache
+    regSW?.addEventListener(
+      'statechange',
+      () => regSW.state === 'activated' && location.reload()
+    )
+  })
+}
+
+registerServiceWorker()
 Store.init()
 Render.init()
 Input.init()
