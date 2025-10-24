572946e278e992312bcf8b14557ab62cb73bb567
Chuck Skoda <charles.skoda@gmail.com>
2025-10-24 00:03:42 -0700

    Starting to reorganize stores around serialization and multiple animations.

--- src/stores/paletteStore.js
+++ src/stores/paletteStore.js
@@ -9,26 +9,27 @@ const paletteItemsEl = document.getElementById('paletteItems')
 const paletteColorsEl = document.getElementById('paletteColors')
 
 const defaultData = Object.seal({
-  selectedColor: 1,
-  selectedPalette: 0,
-
-  palettes: [new Palette()]
+  // This selected palette is for the palette list
+  // In-use pallete is associated with the current animation
+  selectedPalette: null,
+  paletteList: [],
+  paletteMap: {}
 })
 
 export class PaletteStore {
   #data
 
   constructor() {
-    this.#data = { ...defaultData, ...this.#deserialize() }
+    this.#data = { ...defaultData }
+  }
+
+  async init() {
+    await this.#deserialize()
     paletteItemsEl.replaceChildren(...this.paletteListItems)
     paletteColorsEl.replaceChildren(...this.paletteColorItems)
   }
 
   // Accessors
-  get paletteIndex() {
-    return this.#data.selectedPalette
-  }
-
   get colorIndex() {
     return this.palette.selected
   }
@@ -38,7 +39,7 @@ export class PaletteStore {
   }
 
   get palettes() {
-    return this.#data.palettes
+    return this.#data.paletteList
   }
 
   get palette() {
@@ -57,18 +58,50 @@ export class PaletteStore {
     return this.palette.colorItems.slice(1)
   }
 
+  get nextPaletteName() {
+    let num = this.palettes.length
+    let name
+    do {
+      name = `Untitled ${num++}`
+    } while (this.paletteForName(name))
+    return name
+  }
+
+  paletteForName(name) {
+    return this.#data.paletteMap[name]
+  }
+
   // State persistence
   #serialize() {
     const { dataStore } = Store.context
-    const storeData = {
+    const paletteData = {
       paletteState: {
-        selectedColor: this.selectedColor,
-        palettes: this.palettes.map((p) => p.name)
+        paletteList: this.palettes.map((p) => p.name)
       },
       palettes: this.palettes.map((p) => p.serialize())
     }
-    dataStore.save(storeData)
+    dataStore.save(paletteData)
   }
 
-  #deserialize() {}
+  async #deserialize() {
+    const { dataStore } = Store.context
+    const {
+      paletteState: { paletteList },
+      palettes
+    } = await dataStore.loadPaletteData()
+
+    if (!paletteList || !palettes) return
+
+    const paletteMap = Object.fromEntries(
+      paletteData.palettes.map((p) => {
+        const colors = p.colors.map((bytes) => new Color({ bytes }))
+        const palette = new Palette({ name: p.name, colors })
+        return [p.name, palette]
+      })
+    )
+
+    this.#data.paletteList = paletteList.map((name) => paletteMap[name])
+    this.#data.paletteMap = paletteMap
+    this.#data.selectedPalette = this.#data.paletteList[0]
+  }
 }
--- src/types/animation.js
+++ src/types/animation.js
@@ -6,6 +6,7 @@ export class Animation {
 
   #name
   #frames
+  #palette
   #width
   #height
   #item
